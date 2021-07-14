7c3ff4a101d0eff0e84e7154a086d2f3cb381a00
Krishnendu <krishnendu.kalyani1@gmail.com>
2021-07-15 00:12:18 +0530

    c-programming dl

--- js/main.js
+++ js/main.js
@@ -21,3 +21,18 @@ var MenuItems = document.getElementById("MenuItems");
                 MenuItems.style.maxHeight = "0px"
             }
         }
+
+var coll = document.getElementsByClassName("learn-more");
+var i;
+        
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
